4d0c0246802426e7249f629d227ca704f35b385e
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2025-11-05 12:20:05 +0300

    Added build script on Windows

---
+++ build-all.bat
@@ -0,0 +1,7 @@
+mvn -f core/pom.xml install &^
+mvn -f zxcommons/pom.xml install &^
+mvn -DskipTests -f RSyntaxTextArea/pom.xml install &^
+mvn -f microsha/pom.xml package &^
+mvn -f zxspectrum/pom.xml package &^
+cd ide &^
+build-all.bat
---
+++ ide/build-all.bat
@@ -0,0 +1,3 @@
+mvn -f core/pom.xml install &^
+mvn -f i8080.retroide/pom.xml package &^
+mvn -f z80.retroide/pom.xml package
