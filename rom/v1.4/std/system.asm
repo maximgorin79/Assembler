98fffd88f0c5dbd9f5c253838b7bbdc86457dc06
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2026-03-13 12:51:36 +0300

    Project refactored

--- rom/v1.4/std/system.asm
+++ rom/v1.4/std/system.asm
@@ -1,6 +1,8 @@
 ; Delay
 ; bc - delay
 system_delay:
+	push af
+	push bc
 	ld a, b
 	or c
 	jr z, system_delay_exit
@@ -8,12 +10,45 @@ system_delay:
 	dec bc
 	jr system_delay
 system_delay_exit:
+	pop bc
+	pop af
 	ret	
 
 system_random:
+	push af
+	push hl
 	ld a, r			; Load the A register with the refresh register
 	ld l, a			; Copy register A into register L
 	and a, 0x20		; This masking prevents the address we are forming from accessing RAM
 	ld h, a			; Copy register A into register H	
 	ld a,( hl )
+	pop hl
+	pop af
+	ret
+
+clear_memory:
+	push bc
+	push hl
+	push de
+	ld bc, 0xc000
+	ld hl, 0xffff	
+	ld d, h
+	ld e, l
+	dec de
+	ld ( hl ), c
+	lddr	
+	pop de
+	pop hl
+	pop bc
+	ret
+
+set_48k_mode:
+	push af
+	push bc
+	ld a, 0x30
+	ld b, 0x7f
+	ld c, 0xfd
+	out ( c ), a
+	pop bc
+	push af
 	ret
