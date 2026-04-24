f86235b3d17d10d55972a35b7f62f2bd92277986
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2026-04-24 10:55:41 +0300

    Added new items

---
+++ rom/v2.1/items/items16s.asm
@@ -0,0 +1,7 @@
+title_addr:
+	defb "BEST COLLECTION 16", 0
+	
+options_addr:
+	defb "1.Comando Tracer", 0
+	defb "2.Explorer      ", 0
+	defb "3.H.A.T.E.      ", 0
---
+++ rom/v2.1/items/items17s.asm
@@ -0,0 +1,7 @@
+title_addr:
+	defb "BEST COLLECTION 17", 0
+	
+options_addr:
+	defb "1.3W in Paradise", 0
+	defb "2.Robotcop      ", 0
+	defb "3.Sir Fred      ", 0
